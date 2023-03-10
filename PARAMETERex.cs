4297dccf66cabd1dffac7a3bf847538dc86cbf2a
bhiseamar811@gmail.com <bhiseamar811@gmail.com>
2023-03-10 19:24:01 +0530

    Add project files.

---
+++ PARAMETERex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class PARAMETERex
+    {
+        public int i { get; set; }
+        public int j { get; set; }
+       
+        //simple  parameters 
+        public void Addittion(int NUM1, int NUM2)
+        {
+            int result = NUM1 + NUM2;
+
+  
+            Console.WriteLine($"Addition of {NUM1} and {NUM2} is = {result}");
+
+        }
+
+        // Function Complex Parameters 
+        
+
+        public void Multiplication(int NUM1, int NUM2, out int Result)  
+        {
+           
+        Result = NUM1 * NUM2;
+
+            Console.WriteLine($"Multiplication Of {NUM1} and {NUM2} is = {Result}");
+
+        }
+        
+        public void Division(int NUM1, int NUM2, ref int Result)   
+        {
+            Result =  NUM1 / NUM2;
+            Console.WriteLine($" Division Of {NUM1} and {NUM2} is = {Result}");
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+
+            PARAMETERex Parameaterobj = new PARAMETERex();
+            Parameaterobj.Addittion(50, 5);
+
+            PARAMETERex Parameaterobj1 = new PARAMETERex();
+             
+
+            int result=10;
+            Parameaterobj1.Division(999, 9, ref result);
+            Console.WriteLine($"Division of 999 & 9 is = {result}");
+
+
+            /*int Result;
+            Parameaterobj1.Multiplication(545, 689, out Result);
+            Console.WriteLine($"Multiplication of 545 & 689 is = {Result}");*/
+
+        }
+    }
+}
